f13b7a02c993498dc1918c6aa951c044d0bc7374
batra98 <batragaurav2616@gmail.com>
2023-07-15 21:31:57 +0530

    Added a link to the second brain.

--- src/components/icons/formattedIcon.js
+++ src/components/icons/formattedIcon.js
@@ -17,6 +17,7 @@ import {
   IconTwitter,
   IconZap,
 } from '@components/icons';
+import IconBrain from './brain';
 
 const FormattedIcon = ({ name }) => {
   switch (name) {
@@ -50,6 +51,8 @@ const FormattedIcon = ({ name }) => {
       return <IconTwitter />;
     case 'Zap':
       return <IconZap />;
+    case 'Second 🧠':
+      return <IconBrain />;
     default:
       return <IconExternal />;
   }
